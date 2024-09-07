43393bbe9b6cf5dc17bbdadf259a6ea7ab5a046f
Faruk Kodat <farukkodat@gmail.com>
2024-09-07 18:54:24 +0300

    jwtService implemented

---
+++ crm-system-development/crm-system-development/src/main/java/com/kodat/of/crmsystemdevelopment/security/JwtFilter.java
@@ -0,0 +1,29 @@
+package com.kodat.of.crmsystemdevelopment.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    public JwtFilter(JwtService jwtService, UserDetailsService userDetailsService) {
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+    }
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
