a1331a1fdb038086b175c8a843a8ebe6037c2ad8
Faruk Kodat <farukkodat@gmail.com>
2024-09-07 19:05:20 +0300

    UserDetailsService implemented

--- crm-system-development/crm-system-development/src/main/java/com/kodat/of/crmsystemdevelopment/user/entity/UserRepository.java
+++ crm-system-development/crm-system-development/src/main/java/com/kodat/of/crmsystemdevelopment/user/entity/UserRepository.java
@@ -1,4 +1,10 @@
 package com.kodat.of.crmsystemdevelopment.user.entity;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUsername(String username);
 }
