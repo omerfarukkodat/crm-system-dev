9c5460ed9753709cd46d01132e0c0191ef326bc3
Faruk Kodat <farukkodat@gmail.com>
2024-09-07 17:09:22 +0300

    first initial commit

---
+++ crm-system-development/crm-system-development/src/main/java/com/kodat/of/crmsystemdevelopment/CrmSystemDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.kodat.of.crmsystemdevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmSystemDevelopmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmSystemDevelopmentApplication.class, args);
+    }
+
+}
---
+++ crm-system-development/crm-system-development/src/test/java/com/kodat/of/crmsystemdevelopment/CrmSystemDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodat.of.crmsystemdevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmSystemDevelopmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
